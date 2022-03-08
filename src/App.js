1a7a522995b053faf036d4daea315eab53ff6d2b
slamaslam466 <slamaslam466@gmail.com>
2022-03-08 09:10:57 +0530

    fc

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CreateForm from './components/createform'
+import ListUser from './components/listuser';
+
+function App() {
+  return (
+    <>
+      <CreateForm/>
+      <ListUser/>
+    </>
+  );
+}
+
+export default App;
